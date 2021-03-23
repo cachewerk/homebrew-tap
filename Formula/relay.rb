fcb8bc82d54dc973f3ca3cb8b495947e59f1cd0a
Till Krüss <till@kruss.io>
2021-03-23 13:09:30 -0700

    resolve offences

--- Formula/relay.rb
+++ Formula/relay.rb
@@ -2,18 +2,6 @@ class Relay < Formula
   desc "Next-generation caching layer for PHP"
   homepage "https://relaycache.com"
 
-  depends_on "php"
-  depends_on "jemalloc"
-  depends_on "hiredis" => "1.0.0"
-  depends_on "concurrencykit" => "0.7.0"
-  depends_on "liblzf"
-  depends_on "lz4"
-  depends_on "zstd"
-
-  bottle :unneeded
-  
-  # bump
-
   php_ver = Utils.safe_popen_read("#{HOMEBREW_PREFIX}/bin/php-config", "--version").chomp.slice(0, 3)
 
   stable do
@@ -22,21 +10,21 @@ class Relay < Formula
     resource "ext-relay" do
       if Hardware::CPU.arm?
         case php_ver
-          when "8.0"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php8.0-darwin-arm64.tar.gz"
-            # sha256 "..."
-          when "7.4"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php7.4-darwin-arm64.tar.gz"
-            # sha256 "..."
+        when "8.0"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php8.0-darwin-arm64.tar.gz"
+          sha256 "..."
+        when "7.4"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php7.4-darwin-arm64.tar.gz"
+          sha256 "..."
         end
       else
         case php_ver
-          when "8.0"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php8.0-darwin-x86-64.tar.gz"
-            # sha256 "..."
-          when "7.4"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php7.4-darwin-x86-64.tar.gz"
-            # sha256 "..."
+        when "8.0"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php8.0-darwin-x86-64.tar.gz"
+          sha256 "..."
+        when "7.4"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-v0.3.0-php7.4-darwin-x86-64.tar.gz"
+          sha256 "..."
         end
       end
     end
@@ -48,21 +36,31 @@ class Relay < Formula
     resource "ext-relay" do
       if Hardware::CPU.arm?
         case php_ver
-          when "8.0"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php8.0-darwin-arm64.tar.gz"
-          when "7.4"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php7.4-darwin-arm64.tar.gz"
+        when "8.0"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php8.0-darwin-arm64.tar.gz"
+        when "7.4"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php7.4-darwin-arm64.tar.gz"
         end
       else
         case php_ver
-          when "8.0"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php8.0-darwin-x86-64.tar.gz"
-          when "7.4"
-            url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php7.4-darwin-x86-64.tar.gz"
+        when "8.0"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php8.0-darwin-x86-64.tar.gz"
+        when "7.4"
+          url "https://cachewerk.s3.amazonaws.com/relay/relay-dev-php7.4-darwin-x86-64.tar.gz"
         end
       end
     end
   end
+
+  bottle :unneeded
+
+  depends_on "concurrencykit" # v0.7.0+
+  depends_on "hiredis" # v1.0.0+
+  depends_on "jemalloc"
+  depends_on "liblzf"
+  depends_on "lz4"
+  depends_on "php"
+  depends_on "zstd"
 
   def conf_dir
     Pathname(Utils.safe_popen_read("#{HOMEBREW_PREFIX}/bin/php-config", "--ini-dir").chomp)
@@ -71,13 +69,11 @@ class Relay < Formula
   def install
     extensions = Utils.safe_popen_read("#{HOMEBREW_PREFIX}/bin/php", "-m")
 
-    required = ["json", "igbinary", "msgpack"]
-
-    required.each { |extension|
-      unless extensions.match(/^#{extension}/)
-        raise "Relay requires the `#{extension}` extension. Install it using `\033[32mpecl install #{extension}\033[0m`."
+    ["json", "igbinary", "msgpack"].each do |name|
+      unless /^#{name}/.match?(extensions)
+        raise "Relay requires the `#{name}` extension. Install it using `\033[32mpecl install #{name}\033[0m`."
       end
-    }
+    end
 
     resource("ext-relay").stage do
       lib.install "relay.so"
@@ -86,7 +82,7 @@ class Relay < Formula
     # set absolute path to extension
     inreplace "relay.ini", "extension = relay.so", "extension = #{lib}/relay.so"
 
-    # install ini file to `etc/` (won’t overwrite)
+    # install ini file to `etc/` (won't overwrite)
     (etc/"relay").install "relay.ini"
 
     # upsert absolute path to extension if `relay.ini` already existed
