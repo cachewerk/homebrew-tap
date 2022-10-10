75c012e7f829034b0fd45afe64686d3ca9dc89e9
Till Krüss <till@kruss.io>
2022-10-10 11:13:55 -0700

    remove comment

--- Formula/relay@7.4.rb
+++ Formula/relay@7.4.rb
@@ -65,8 +65,6 @@ class RelayAT74 < Formula
 
       # inject UUID into binary
       `LC_ALL=C /usr/bin/sed -i '' s/00000000-0000-0000-0000-000000000000/#{SecureRandom.uuid}/ relay.so`
-
-      # inject UUID into old binary
       `LC_ALL=C /usr/bin/sed -i '' s/BIN:31415926-5358-9793-2384-626433832795/BIN:#{SecureRandom.uuid}/ relay.so`
 
       # relink dependencies
